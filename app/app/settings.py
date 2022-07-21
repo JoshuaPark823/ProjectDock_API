8ddfee53e94420e231ba4f2c0f71340e0047db99
Joshua Park <joshuapark823@gmail.com>
2022-07-20 23:10:07 -0400

    cors policy

--- app/app/settings.py
+++ app/app/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS.extend(
     )
 )
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -109,6 +110,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
